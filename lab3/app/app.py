053f367cbfcce63a8f14036de19b3fe56a65a763
Валерий Денисов <valerad2212@mail.ru>
2024-04-09 23:21:41 +0300

    add lab 3 final

--- lab3/app/app.py
+++ lab3/app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, session, request, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
+from flask import Flask, render_template, session, request, redirect, url_for, flash
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
 app = Flask(__name__)
 application = app
 
@@ -7,12 +7,17 @@ application = app
 app.config.from_pyfile('config.py')
 
 login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message = 'Для доступа к этой странице нужно залогиниться'
+login_manager.login_message_category = 'alert-warning'
+
+
 login_manager.init_app(application)
 
 class User(UserMixin):
     def __init__(self, login, user_id):
         self.login = login
-        self.id = user_id
+        self.id = user_id 
     
 
 @login_manager.user_loader
@@ -37,17 +42,30 @@ def login():
     if request.method == "POST":    
         name = request.form.get("login", False)
         password = request.form.get("password", False)
+        remember_me = request.form.get('remember_me') == "on"
         db = user_list()
-        text = ""
-        category = ""
+        text = "Ошибка логина"
+        category = "alert-danger"
         for record in db:
             if record["login"] == name and record["password"] == password:
                 user = User(record["login"], record["id"])
-                login_user(user)
-                return redirect(url_for("index"))
+                login_user(user, remember=remember_me)
+                flash("Успешно", "alert-success")
+            
+                return redirect(request.args.get("next") or url_for("index"))
+            
+        flash("Введены некорректные данные", "alert-danger")
         
     return render_template('flask_login.html')
 
+
+@app.route('/secret')
+@login_required
+def secret():
+    # if not  current_user.is_authenticated:
+    #     flash("Для доступа к этой странице нужно залогиниться", "alert-info")
+    #     return redirect(url_for("login")) 
+    return render_template("secret.html")
 
 
 @app.route('/logout')
